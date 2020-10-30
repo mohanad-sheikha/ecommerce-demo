0f45466b43027f2e65aff3e5de7c3f4bb057e651
Mohanad Abu Sheikha <you@example.com>
2020-10-30 17:57:33 -0400

    added more test products

--- src/mobx/ProductsStore.js
+++ src/mobx/ProductsStore.js
@@ -11,11 +11,23 @@ import { observable, action, computed } from 'mobx'
 class ProductsStore
 {
 	@observable byId = {
-		'1z8sj24': {
-			id: '1z8sj24',
+		'151zzxcasd': {
+			id: '151zzxcasd',
 			price: 850.99,
 			name: '16-inch Macbook Pro (2020)',
 			imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/71h6PpGaz9L._AC_SL1500_.jpg'
+		},
+		'zz1232ff': {
+			id: 'zz1232ff',
+			price: 899.99,
+			name: '17-inch HP Omen',
+			imageUrl: 'https://store.hp.com/app/assets/images/product/5VX46AV_1/center_facing.png?_=1562585895398&imwidth=270&imdensity=1'
+		},
+		'2s82kdyzxA': {
+			id: '2s82kdyzxA',
+			price: 1030.99,
+			name: '15.6-inch XPS 15',
+			imageUrl: 'https://i.dell.com/sites/csimages/Video_Imagery/all/xps_7590_touch.png'
 		}
 	}
 	@observable isLoading = false
