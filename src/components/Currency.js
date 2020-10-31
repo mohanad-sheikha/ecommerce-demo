1eb4eec2ee351ff02c423ffaeca56e20557f1bfe
Mohanad Abu Sheikha <you@example.com>
2020-10-31 02:58:40 -0400

    added forwardRef to allow styling wrapper

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -6,16 +6,16 @@ const Wrapper = styled.span`
 	font-family: 'Overpass', sans-serif;
 `
 
-export default function Currency ({ value })
+const Currency = React.forwardRef(({ value, ...rest }, ref) =>
 {
 	const getFormattedCurrency = () => new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(value)
 
-	return (
-		<Wrapper>{ getFormattedCurrency() }</Wrapper>
-	)
-}
+	return <Wrapper { ...rest } ref={ ref }>{ getFormattedCurrency() }</Wrapper>
+})
 
 Currency.propTypes =
 {
 	value: PropTypes.number.isRequired
 }
+
+export default Currency
