9b7a6065b739aa47ee20fd3e9bdbc58b57454a07
Mohanad Abu Sheikha <you@example.com>
2020-10-31 02:58:22 -0400

    added global border style css variables

--- src/index.js
+++ src/index.js
@@ -15,6 +15,13 @@ const GlobalStyle = createGlobalStyle`
 		outline: none !important;
 		border: none !important;
 	}
+	:root
+	{
+		--moe-border-style: solid;
+		--moe-border-color: hsl(0, 0%, 80%);
+		--moe-border-small: 1px var(--moe-border-style) var(--moe-border-color);
+		--moe-border-medium: 2px var(--moe-border-style) var(--moe-border-color);
+	}
 `
 
-ReactDOM.render(<><App /><GlobalStyle /></>, document.getElementById('app'))
+ReactDOM.render(<React.Fragment><App /><GlobalStyle /></React.Fragment>, document.getElementById('app'))
