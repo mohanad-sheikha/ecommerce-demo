6a534fc5adf7aabfb5ac917205048d87aa2631e4
Mohanad Abu Sheikha <you@example.com>
2020-10-31 02:58:51 -0400

    renamed component

---
+++ src/components/ProductSpecifications.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+
+const Wrapper = styled.dl`
+	display: grid;
+	grid-template-columns: min-content auto;
+	grid-row-gap: 0.5rem;
+	grid-column-gap: 2rem;
+	margin: 0;
+	align-items: baseline;
+`
+const Label = styled.dt`
+	font-weight: 400;
+	color: var(--bs-secondary);
+	white-space: nowrap;
+`
+const Value = styled.dd`
+	margin: 0;
+	line-break: anywhere;
+`
+
+const ProductSpecifications = ({ product }) =>
+{
+	const humanLabels = {
+		'id': 'Identifier',
+		'price': 'Price',
+		'name': 'Name',
+		'imageUrl': 'Image URL',
+	}
+
+	return (
+		<Wrapper>
+			{ product && Object.entries(product).map(([ label, value ]) => (
+				<React.Fragment key={ label }>
+					<Label>{ humanLabels[ label ] || label }</Label>
+					<Value>{ value }</Value>
+				</React.Fragment>
+			)) }
+		</Wrapper>
+	)
+}
+
+ProductSpecifications.propTypes =
+{
+	product: PropTypes.object.isRequired
+}
+
+export default ProductSpecifications
