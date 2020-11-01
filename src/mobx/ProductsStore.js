b390bb3b8fbdd5a6ae95308cf2379b3d0af4d8c3
Mohanad Abu Sheikha <you@example.com>
2020-11-01 02:03:52 -0500

    created test data

--- src/mobx/ProductsStore.js
+++ src/mobx/ProductsStore.js
@@ -2,34 +2,18 @@
  * @typedef {Object} Product
  * @property {Number} id
  * @property {Number} price
+ * @property {Number} originalPrice
+ * @property {String} size
  * @property {String} name
  * @property {String} imageUrl
  */
 
 import { observable, action, computed } from 'mobx'
+import testData from '../data.json'
 
 class ProductsStore
 {
-	@observable byId = {
-		'151zzxcasd': {
-			id: '151zzxcasd',
-			price: 850.99,
-			name: '16-inch Macbook Pro i9 (2020)',
-			imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/71h6PpGaz9L._AC_SL1500_.jpg'
-		},
-		'zz1232ff': {
-			id: 'zz1232ff',
-			price: 899.99,
-			name: '17-inch HP Omen',
-			imageUrl: 'https://store.hp.com/app/assets/images/product/5VX46AV_1/center_facing.png?_=1562585895398&imwidth=270&imdensity=1'
-		},
-		'2s82kdyzxA': {
-			id: '2s82kdyzxA',
-			price: 1030.99,
-			name: '15.6-inch XPS 15',
-			imageUrl: 'https://i.dell.com/sites/csimages/Video_Imagery/all/xps_7590_touch.png'
-		}
-	}
+	@observable byId = {}
 	@observable isLoading = false
 
 	constructor ()
@@ -50,7 +34,7 @@ class ProductsStore
 	initialize ()
 	{
 		this.isLoading = true
-		// Get initial state here
+		this.byId = testData.reduce((collection, product) => ({ ...collection, [ product.id ]: { ...product } }), {})
 		this.isLoading = false
 	}
 }
