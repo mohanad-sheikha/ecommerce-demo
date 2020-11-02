1f55c0d112eefdf376a409e7ae75631c441d14ae
Mohanad Abu Sheikha <mohanadsheikha@gmail.com>
2020-11-02 13:05:59 -0500

    added error handling for missing image

--- src/components/ProductImage.js
+++ src/components/ProductImage.js
@@ -1,18 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
 
-const Wrapper = styled.img`
+const Wrapper = styled.div`
+	width: 100%;
+	height: 100%;
+	position: relative;
+`
+const Image = styled.img`
 	width: 100%;
 	height: 100%;
 	object-fit: contain;
 	margin: auto;
 `
+const ErrorWrapper = styled.div`
+	display: grid;
+	grid-auto-flow: row;
+	justify-content: center;
+	align-content: center;
+	position: absolute;
+	top: 0; right: 0; bottom: 0; left: 0;
+	background-color: white;
+	color: var(--bs-secondary);
+	pointer-events: none;
+`
+
+const ErrorOverlay = () => <ErrorWrapper>
+	<i className='fa fa-2x fa-file-image'></i>
+</ErrorWrapper>
 
 const ProductImage = ({ product: { imageUrl } }) =>
 {
+	const [ hasError, setHasError ] = useState(false)
+
+	useEffect(() => { setHasError(false) }, [ imageUrl ])
+
 	return (
-		<Wrapper src={ imageUrl }></Wrapper>
+		<Wrapper>
+			<Image src={ imageUrl } onError={ () => { setHasError(true) } } />
+			{ hasError && <ErrorOverlay /> }
+		</Wrapper>
 	)
 }
 
